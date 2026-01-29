54e0102e3f2b837fad920d7bdba66dad53f197a2
TouristeAG <leonardo.mondada@gmail.com>
2026-01-30 00:46:17 +0100

    Bug fixes for updates

--- app/build/generated/source/buildConfig/debug/com/eventmanager/app/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/eventmanager/app/BuildConfig.java
@@ -7,7 +7,7 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.eventmanager.app";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 3;
+  public static final int VERSION_CODE = 4;
   public static final String VERSION_NAME = "0.9.3";
   // Field from default config.
   public static final String UPDATE_FALLBACK_STORE_URL = "https://play.google.com/store/apps/details?id=com.eventmanager.app";
